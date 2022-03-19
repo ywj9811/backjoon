1c75edc557e517e28408729b9486a0996aa0fbdf
ywj9811 <ywj9811@naver.com>
2022-03-19 23:05:54 +0900

    수정

--- backjoon/src/backjoon_function/Self.java
+++ backjoon/src/backjoon_function/Self.java
@@ -4,32 +4,33 @@ import java.util.ArrayList;
 
 public class Self {
 	static int sum(int a) {
-		if(a > 0 && a < 10) {
-			return a + a;
+		int sum = a;
+		while(a != 0) {
+			sum = sum + (a % 10);
+			a = a/10;
 		}
-		else if(a >= 10 && a < 100) {
-			return a + (a/10);
-		}
-		else if(a >= 100 && a < 1000) {
-			return a + (a/10 - a/100) + (a/100);
-		}
-		else
-			return a + (a/10 - a/100 - a/1000) + (a/100 - a/1000) + (a/1000);
+		
+		return sum;
 	}
 	
 	public static void main(String[] args) {
 		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<Integer> arr2 = new ArrayList<Integer>();
 		for(int i = 0; i < 10000; i++) {
-			arr.add(i+1);
+			arr.add((i+1));
+			arr2.add(i+1);
 		}
 		
 		for(int i = 0; i < arr.size(); i++) {
-			for(int j = 0; j < arr.size(); j++) {
-				if(Self.sum(arr.get(i)) == arr.get(j)) {
-					arr.remove(j);
+			for(int j = 0; j < arr2.size(); j++) {
+				if(Self.sum(arr.get(i)) == arr2.get(j)) {
+					arr2.remove(j);
+					break;
 				}
 			}
 		}
-		System.out.println(arr);
+		for(int i = 0; i < arr2.size(); i++) {
+			System.out.println(arr2.get(i));
+		}
 	}
 }
