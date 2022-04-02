6ac5605664dac9b9ace8b4235b99b5837630f889
ywj9811 <ywj9811@naver.com>
2022-04-03 00:41:51 +0900

    baekjoon

---
+++ backjoon/src/baekjoon_greedy/B_1946_2.java
@@ -0,0 +1,10 @@
+package baekjoon_greedy;
+
+import java.util.Scanner;
+
+public class B_1946_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	}
+}
