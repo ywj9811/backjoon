85dca55e0f43909b558bb72dc7cd32378a7c6f89
ywj9811 <ywj9811@naver.com>
2022-05-20 15:21:33 +0900

    진행중

--- backjoon/src/baekjoon_sort/B_10814.java
+++ backjoon/src/baekjoon_sort/B_10814.java
@@ -1,32 +1,34 @@
 package baekjoon_sort;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class B_10814 {
-	static swap()
-	
 	public static void main(String[] args) throws Exception {
 		//나이가 증가하는 순, 나이가 같으면 먼저 가입한 순으로.
 		//가입한 순서대로 데이터가 주어지게 된다.
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = sc.nextInt(); //몇명을 입력받을 것인가.
+		int n = Integer.parseInt(br.readLine()); //몇명을 입력받을 것인가.
 		
-		String user[][] = new String[n][2];
+		ArrayList<String> user = new ArrayList<String>();
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < 2; j++) {
-				user[i][j] = sc.next();
-			}
-		}
-		//[][0]에는 나이 [][1]에는 이름
+		for(int i = 0; i < n; i++)
+			user.add(br.readLine());
 		
-		Arrays.sort(user, (o1, o2) -> {
-			if(o1[0] == o2[0]) {
-				return (o1[1], o2[1]);
-			}
-		}
+		//user에 순서대로 들어있다.
+		
+		
+		
+		
+//		String user[][] = new String[n][2];
+//		
+//		for(int i = 0; i < n; i++) {
+//			for(int j = 0; j < 2; j++) {
+//				user[i][j] = sc.next();
+//			}
+//		}
+
 	}
 }
